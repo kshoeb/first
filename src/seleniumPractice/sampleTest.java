f960344452540a88cf52cd1864879366507f9741
kshoeb <ksa.shoeb@gmail.com>
2018-08-30 12:45:10 +0530

    SampleTest java file created

---
+++ src/seleniumPractice/sampleTest.java
@@ -0,0 +1,16 @@
+package seleniumPractice;
+
+public class sampleTest {
+
+	public static void main(String[] args) {
+
+		int num[] ={20,10,20,40};
+		System.out.println("Display in loop");
+		for(int i=0;i<num.length;i++)
+		{
+			System.out.println("Num : " + num[i]);
+		}
+
+	}
+
+}
